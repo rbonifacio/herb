c644595a607baca5e53daf65a1b7bf94ea794b4d
Denis Albuquerque <denisalbuquerque@gmail.com>
2013-11-14 00:17:59 -0200

    Novas funções para tratar Requests e Responses

--- server.hs
+++ server.hs
@@ -1,16 +1,47 @@
 module Server where
 -- HERB server's functions 
 
-import System.IO (Handle(..), hPutStr)
+import System.IO (Handle(..), hPutStr, hClose, hGetContents)
+import Data.Time
 
 import DataDef
 
 dispatch :: Handle -> String -> IO ()
 dispatch handle hostname = do  
     putStrLn $ "Request from " ++ hostname
-    reply handle
+    request <- fmap lines (hGetContents handle) --[String]
+    putStrLn $ head request
+    let req = parseRequest request
+    let response = buildResponse req
+    reply handle response
+    return ()
 
-reply :: Handle -> IO ()
-reply handle = do
-    let resp = Response ResponseHeaders {statusCode = "200", resDate = "Data/Hora", resConType = "application/json" } "{ JSON BODY }"
-    hPutStr handle $ show resp
+parseRequest :: [String] -> Request
+parseRequest lista = case (head (words (head lista))) of
+    "GET" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = "temp", rMethod = GET, rParams = []}
+    "POST" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = "temp", rMethod = POST, rParams = []}
+    "PUT" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = "temp", rMethod = PUT, rParams = []}
+    "DELETE" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = "temp", rMethod = DELETE, rParams = []}
+
+buildResponse :: Request -> Response
+buildResponse (Request hd bd)
+    | rMethod(bd) == GET = Response ResponseHeaders { statusCode = "200", resDate = dt, resConType = "application/json" } "{ response for GET }"
+    | rMethod(bd) == POST = Response ResponseHeaders { statusCode = "200", resDate = dt, resConType = "application/json" } "{ response for POST }"
+    | rMethod(bd) == PUT = Response ResponseHeaders { statusCode = "200", resDate = dt, resConType = "application/json" } "{ response for PUT }"
+    | rMethod(bd) == DELETE = Response ResponseHeaders { statusCode = "200", resDate = dt, resConType = "application/json" } "{ response for DELETE }"
+    where dt = getTime
+
+getTime :: String
+getTime =
+    --do { 
+    --; cT <- fmap show getCurrentTime
+    --; return cT
+    --}
+    "2013 - 11 - .."  
+
+reply :: Handle -> Response -> IO ()
+reply handle response = do
+    hPutStr handle $ show response
+    hClose handle
+    return ()
+    
