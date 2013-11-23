01d54ebe7acc09ac38045a54353639652b141422
Denis Albuquerque <denisalbuquerque@gmail.com>
2013-11-23 15:55:45 -0200

    Mudanças na construção das respostas

--- datadef.hs
+++ datadef.hs
@@ -5,6 +5,7 @@ data RequestMethod = GET
                    | POST 
                    | PUT
                    | DELETE
+                   | UNDEFINED
                    deriving (Show, Eq)
 
 data RequestHeaders = RequestHeaders {
@@ -16,7 +17,7 @@ data RequestBody = RequestBody {
                     rURI :: String,             -- Request Universal Resourse Identifier
                     rMethod :: RequestMethod,   -- Request Type
                     rParams :: [(String,String)]--  Parâmetros (chave,valor)
-}    
+}
 
 data Request = Request RequestHeaders RequestBody
 
@@ -31,10 +32,10 @@ type ResponseBody = String
 data Response = Response ResponseHeaders ResponseBody
 
 instance Show ResponseHeaders where
-    show resp = "Status Code: " ++ statusCode(resp) ++ "\nDate: " ++ resDate(resp) ++ "\nContent-Type: " ++ resConType(resp)
+    show resp = "HTTP/1.1 " ++ statusCode(resp) ++ reasonPhrase(statusCode(resp)) ++ "\nDate: " ++ resDate(resp) ++ "\nContent-Type: " ++ resConType(resp)
 
 instance Show Response where
-    show (Response headers body) = show(headers) ++ "\n" ++ body
+    show (Response headers body) = show(headers) ++ "\n\n" ++ body
 
 instance Show Request where
     show (Request headers body) = show(headers) ++ "\n" ++ show(body)
@@ -51,4 +52,48 @@ stringToMethod s = case s of
     "POST" -> POST
     "PUT" -> PUT
     "DELETE" -> DELETE
+    _ -> UNDEFINED
+
+reasonPhrase :: String -> String
+reasonPhrase statusCode = case statusCode of
+    "100" -> " Continue"
+    "101" -> " Switching Protocols"
+    "200" -> " OK"
+    "201" -> " Created"
+    "202" -> " Accepted"
+    "203" -> " Non-Authoritative Information"
+    "204" -> " No Content"
+    "205" -> " Reset Content"
+    "206" -> " Partial Content"
+    "300" -> " Multiple Choices"
+    "301" -> " Moved Permanently"
+    "302" -> " Found"
+    "303" -> " See Other"
+    "304" -> " Not Modified"
+    "305" -> " Use Proxy"
+    "307" -> " Temporary Redirect"
+    "400" -> " Bad Request"
+    "401" -> " Unauthorized"
+    "402" -> " Payment Required"
+    "403" -> " Forbidden"
+    "404" -> " Not Found"
+    "405" -> " Method Not Allowed"
+    "406" -> " Not Acceptable"
+    "407" -> " Proxy Authentication Required"
+    "408" -> " Request Time-out"
+    "409" -> " Conflict"
+    "410" -> " Gone"
+    "411" -> " Length Required"
+    "412" -> " Precondition Failed"
+    "413" -> " Request Entity Too Large"
+    "414" -> " Request-URI Too Large"
+    "415" -> " Unsupported Media Type"
+    "416" -> " Requested range not satisfiable"
+    "417" -> " Expectation Failed"
+    "500" -> " Internal Server Error"
+    "501" -> " Not Implemented"
+    "502" -> " Bad Gateway"
+    "503" -> " Service Unavailable"
+    "504" -> " Gateway Time-out"
+    "505" -> " HTTP Version not supported"
 
