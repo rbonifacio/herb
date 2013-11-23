b16b4579d08fc122cd7847b621edb973d18eca77
Denis Albuquerque <denisalbuquerque@gmail.com>
2013-11-23 15:57:26 -0200

    Faxina no código.

--- server.hs
+++ server.hs
@@ -8,40 +8,39 @@ import DataDef
 
 dispatch :: Handle -> String -> IO ()
 dispatch handle hostname = do  
-    putStrLn $ "Request from " ++ hostname
-    request <- fmap lines (hGetContents handle) --[String]
-    putStrLn $ head request
-    let req = parseRequest request
-    let response = buildResponse req
+    putStrLn $ "*** Request from " ++ hostname ++ "***"
+    rqst <- fmap lines (hGetContents handle) --[String]
+    let headRqst = head rqst
+    putStrLn $ headRqst ++ "\n"
+    let parsedRqst = parseRequest $ headRqst
+    putStrLn $ "*** REQUEST ***\n" ++ (show parsedRqst) ++ "\n"
+    let response = buildResponse parsedRqst
+    putStrLn $ "*** RESPONSE ***\n" ++ (show response) ++ "\n"
     reply handle response
     return ()
 
-parseRequest :: [String] -> Request
-parseRequest lista = case (head (words (head lista))) of
-    "GET" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = "temp", rMethod = GET, rParams = []}
-    "POST" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = "temp", rMethod = POST, rParams = []}
-    "PUT" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = "temp", rMethod = PUT, rParams = []}
-    "DELETE" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = "temp", rMethod = DELETE, rParams = []}
+parseRequest :: String -> Request
+parseRequest lista = case (head (words (lista))) of
+    "GET" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = (words lista) !! 1, rMethod = GET, rParams = []}
+    "POST" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = (words lista) !! 1, rMethod = POST, rParams = []}
+    "PUT" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = (words lista) !! 1, rMethod = PUT, rParams = []}
+    "DELETE" -> Request RequestHeaders { reqConType = "application/json", rAccept = "*/*"} RequestBody { rURI = (words lista) !! 1, rMethod = DELETE, rParams = []}
+    _  ->  Request RequestHeaders { reqConType = "", rAccept = ""}  RequestBody { rURI = (words lista) !! 1, rMethod = UNDEFINED, rParams = [] }
 
 buildResponse :: Request -> Response
 buildResponse (Request hd bd)
-    | rMethod(bd) == GET = Response ResponseHeaders { statusCode = "200", resDate = dt, resConType = "application/json" } "{ response for GET }"
-    | rMethod(bd) == POST = Response ResponseHeaders { statusCode = "200", resDate = dt, resConType = "application/json" } "{ response for POST }"
-    | rMethod(bd) == PUT = Response ResponseHeaders { statusCode = "200", resDate = dt, resConType = "application/json" } "{ response for PUT }"
-    | rMethod(bd) == DELETE = Response ResponseHeaders { statusCode = "200", resDate = dt, resConType = "application/json" } "{ response for DELETE }"
-    where dt = getTime
-
-getTime :: String
-getTime =
-    --do { 
-    --; cT <- fmap show getCurrentTime
-    --; return cT
-    --}
-    "2013 - 11 - .."  
+    | rMethod(bd) == GET = Response ResponseHeaders { statusCode = "200", resDate = "", resConType = "application/json" } "{ response for GET }"
+    | rMethod(bd) == POST = Response ResponseHeaders { statusCode = "200", resDate = "", resConType = "application/json" } "{ response for POST }"
+    | rMethod(bd) == PUT = Response ResponseHeaders { statusCode = "200", resDate = "", resConType = "application/json" } "{ response for PUT }"
+    | rMethod(bd) == DELETE = Response ResponseHeaders { statusCode = "200", resDate = "", resConType = "application/json" } "{ response for DELETE }"
+    | rMethod(bd) == UNDEFINED = Response ResponseHeaders { statusCode = "405", resDate = "", resConType = "application/json" } "{ UNDEFINED METHOD }"
 
 reply :: Handle -> Response -> IO ()
-reply handle response = do
-    hPutStr handle $ show response
+reply handle response@(Response hdr bdy) = do
+    cT <- fmap show getCurrentTime  
+    let respHeader = hdr { resDate = cT }
+    let rsp = Response respHeader bdy
+    hPutStr handle $ show rsp
     hClose handle
     return ()
     
