ce8b5bcc09d948418f383c310b67b2c4854a6ead
Denis Albuquerque <denisalbuquerque@gmail.com>
2013-11-28 11:27:05 -0200

    Correções na captura dos Requests.
    Adcionadas funcionalidades para verificar se um request possui body e
    captura desde body em uma string.

--- main.hs
+++ main.hs
@@ -1,4 +1,4 @@
--- HERB server 0.1
+-- HERB server
 
 -- to compile: ghc -threaded --make main.hs
 -- usage: ./main [server port] +RTS -N2
@@ -7,7 +7,8 @@ module Main where
 
 import System.Environment (getArgs)
 import Network (listenOn, PortID(..), PortNumber(..), accept)
-import System.IO (hSetBuffering, BufferMode(..), Handle(..))
+import Network.Socket.Internal (withSocketsDo)
+import System.IO (hSetBuffering, BufferMode(..), Handle(..), hClose)
 import Control.Monad (forever)
 import Control.Concurrent (forkIO)
 
@@ -15,15 +16,18 @@ import Server
 
 -- loads the HERB server 
 main :: IO ()
-main = do
+main = withSocketsDo $ do
+    -- TODO: tratar opcoes de entrada. Numero da Porta, Arquivo de Configuracao, etc.
     port <- fmap lerPorta getArgs
     socket <- listenOn $ PortNumber port 
     putStrLn $ "HERB Server Started. Listening on port " ++ show port
     forever $ do
-        (handle, hostName, clientPortNumber) <- accept socket
-        hSetBuffering handle NoBuffering
-        forkIO $ dispatch handle hostName
+        (handle, hostName, clntPrtNmbr) <- accept socket
+        hSetBuffering handle NoBuffering -- line-buffering, block-buffering or no-buffering
+        forkIO $ dispatch handle hostName >> hClose handle
+
 
 lerPorta :: [String] -> PortNumber
 lerPorta []  = 8080 
 lerPorta [p] = fromIntegral $ read p        
+
--- server.hs
+++ server.hs
@@ -1,71 +1,104 @@
+{-# LANGUAGE DoAndIfThenElse #-} 
+
 module Server where
 -- HERB server's functions 
 
-import System.IO (Handle(..), hPutStr, hClose, hGetContents)
+import System.IO (Handle(..), hPutStr, hClose, hGetLine, hGetChar)
 import System.IO.Unsafe (unsafePerformIO)
-import Data.Time
+import Data.Time (getCurrentTime)
+import Data.Maybe (fromJust)
 
 import DataDef
 
+version :: String
+version = "HERB Server 0.1"
+
 dispatch :: Handle -> String -> IO ()
 dispatch handle hostname = do  
-    putStrLn $ "*** Request from " ++ hostname ++ "***"
-    -- Read Request from Client
-    rqst <- fmap lines (hGetContents handle) --[String]
-    let headRqst = head rqst
-    putStrLn $ headRqst ++ "\n"
-    -- Parse Request
-    let parsedRqst = parseRequest $ headRqst
-    putStrLn $ "*** REQUEST ***\n" ++ (show parsedRqst) ++ "\n"
-    -- Generate Response
-    let response = buildResponse parsedRqst
-    putStrLn $ "*** RESPONSE ***\n" ++ (show response) ++ "\n"
-    -- Send Response Back to client
-    reply handle response
-    return ()
+    putStrLn $ (unsafePerformIO $ fmap show getCurrentTime) ++ " : Request from " ++ hostname
+    let getLines = repeat (hGetLine handle) -- [IO String]
+    lns <- takeLines getLines               -- [String]
+    let parsedRqst@(Request r h b) = parseRequest lns
+    bdy <- getBody handle (getContLen parsedRqst) (return [])  
+    let updatedRqst = (Request r h bdy)
+    -- (verbose) putStrLn $ "*** REQUEST ***\n" ++ (show updatedRqst)
+    let response = buildResponse updatedRqst
+    -- (verbose) putStrLn $ "*** RESPONSE ***\n" ++ (show response)
+    reply handle response 
 
-parseRequest :: String -> Request
-parseRequest lista = 
-    let uri =  (words lista) !! 1
-        version = "HTTP/1.1"
-        contype = "application/json"
-        acpt = "*/*"
-        rqst :: RequestMethod -> Request
-        rqst m = Request 
-                 Rqst { rURI = uri, rMethod = m, rVersion = version } 
-                 RequestHeaders { reqConType = contype, rAccept = acpt } 
-                 RequestBody { rParams = [] }
-    in case (head (words (lista))) of
-        "GET" -> rqst GET
-        "POST" -> rqst POST
-        "PUT" -> rqst PUT
-        "DELETE" -> rqst DELETE
-        _ -> rqst UNDEFINED
+-- gets the size of a request body 
+getContLen :: Request -> Int
+getContLen request@(Request rqst hdr bdy) =
+    let len = lookup "Content-Length" hdr
+    in case len of 
+        (Just n) -> read n :: Int
+        (Nothing) -> 0
 
+-- gets the string that correspond to the body of a request
+getBody :: Handle -> Int -> IO String -> IO String
+getBody handle 0 bdy = bdy
+getBody handle len bdy = do 
+    c <- hGetChar handle
+    cs <- getBody handle (len-1) bdy
+    return (c:cs)
+
+takeLines :: [IO String] -> IO [String]
+takeLines getLines = do
+    x <- head getLines
+    case show x of
+        "\"\\r\"" -> return []
+        "\"\"" -> return []
+        _ -> (takeLines (tail getLines)) >>= \xs -> return (x:xs)
+
+parseRequest :: [String] -> Request
+parseRequest lns = 
+    let rqst = words (head lns)
+        uri =  rqst !! 1
+        version = rqst !! 2
+        hdrs = parseRqstHdrs (tail lns) []
+        request :: RequestMethod -> Request
+        request m = Request 
+                    Rqst { rURI = uri, rMethod = m, rVersion = version } 
+                    hdrs
+                    []
+    in case (head rqst) of
+        "GET" -> request GET
+        "POST" -> request POST
+        "PUT" -> request PUT
+        "DELETE" -> request DELETE
+        _ -> request UNDEFINED
+
+parseRqstHdrs :: [String] -> RequestHeaders -> RequestHeaders -- [(String,String)]
+parseRqstHdrs [] parsedHrds = parsedHrds
+parseRqstHdrs (l:lns) parsedHrds = parseRqstHdrs lns $ parsedHrds++[(k,v)]
+    where k = takeWhile (/=':') l
+          v = tail $ dropWhile (/=' ') l
+
+-- Generate response
 buildResponse :: Request -> Response
-buildResponse (Request rqst hdr bdy) = 
-    let date = unsafePerformIO $ fmap show getCurrentTime
-        server = "HERB Server 0.1"
-        contype = "application/json"
+buildResponse request@(Request rqst hdr bdy) = 
+    let contype = "application/json"
+        getDate = unsafePerformIO $ fmap show getCurrentTime
     in case (rMethod(rqst)) of 
         GET -> Response
-               ResponseHeaders { statusCode = "200", resDate = date, resConType = contype, resServer = server}
-               "{ response for GET }"
+               ResponseHeaders { statusCode = "200", resDate = getDate, resConType = contype, resServer = version}
+               ( "{ request }\n" ++ show request )
         POST -> Response
-               ResponseHeaders { statusCode = "200", resDate = date, resConType = contype, resServer = server}
-               "{ response for POST }"
+               ResponseHeaders { statusCode = "201", resDate = getDate, resConType = contype, resServer = version}
+               ( "{ request }\n" ++ show request )
         PUT -> Response
-               ResponseHeaders { statusCode = "200", resDate = date, resConType = contype, resServer = server}
-               "{ response for PUT }"
+               ResponseHeaders { statusCode = "200", resDate = getDate, resConType = contype, resServer = version}
+               ( "{ request }\n" ++ show request )
         DELETE -> Response
-               ResponseHeaders { statusCode = "200", resDate = date, resConType = contype, resServer = server}
-               "{ response for DELETE }"
+               ResponseHeaders { statusCode = "200", resDate = getDate, resConType = contype, resServer = version}
+               ( "{ request }\n" ++ show request )
         UNDEFINED -> Response
-               ResponseHeaders { statusCode = "405", resDate = date, resConType = contype, resServer = server}
-               "{ response for GET }"
+               ResponseHeaders { statusCode = "405", resDate = getDate, resConType = contype, resServer = version}
+               ( "{ request }\n" ++ show request )
 
+-- Send Response back to client
 reply :: Handle -> Response -> IO ()
-reply handle rsp = do
-    hPutStr handle $ show rsp
-    hClose handle
+reply handle rsp@(Response hdr bdy) = do
+    hPutStr handle $ show rsp 
+    hPutStr handle $ "\n{ response }\n" ++ show hdr
     return ()
