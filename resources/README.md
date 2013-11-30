a788b3c548eeaa48f39e4cefc9fb75b6c27c6ecb
Denis Albuquerque <denisalbuquerque@gmail.com>
2013-11-30 21:05:30 -0200

    Inclusão de funções para processar requests do tipo GET e POST, além de funções acessórias para construir documentos JSON.

---
+++ resources/README.md
@@ -0,0 +1,3 @@
+Diretório para armazenamento de recursos:
+* Eventos (arquivos)
+* Conjuntos de Eventos (diretórios)
--- server.hs
+++ server.hs
@@ -1,12 +1,17 @@
-{-# LANGUAGE DoAndIfThenElse #-} 
+{-# LANGUAGE DoAndIfThenElse #-}
 
 module Server where
 -- HERB server's functions 
 
-import System.IO (Handle(..), hPutStr, hClose, hGetLine, hGetChar)
+import System.IO (Handle(..), hPutStr, hGetLine, hGetChar)
 import System.IO.Unsafe (unsafePerformIO)
 import Data.Time (getCurrentTime)
-import Data.Maybe (fromJust)
+import System.Posix.Files.ByteString (getFileStatus, isDirectory, fileExist)
+import System.Directory (getDirectoryContents)
+import Data.Aeson (toJSON, ToJSON(..), (.=), object, encode)
+import qualified Data.Text as T (Text(..), pack)
+import qualified Data.ByteString.Char8 as B (pack)
+import qualified Data.ByteString.Lazy.Char8 as BL (unpack)
 
 import DataDef
 
@@ -22,7 +27,8 @@ dispatch handle hostname = do
     bdy <- getBody handle (getContLen parsedRqst) (return [])  
     let updatedRqst = (Request r h bdy)
     -- (verbose) putStrLn $ "*** REQUEST ***\n" ++ (show updatedRqst)
-    let response = buildResponse updatedRqst
+    processedRqst@(code,body) <- proccessRequest updatedRqst
+    let response = buildResponse processedRqst
     -- (verbose) putStrLn $ "*** RESPONSE ***\n" ++ (show response)
     reply handle response 
 
@@ -74,31 +80,74 @@ parseRqstHdrs (l:lns) parsedHrds = parseRqstHdrs lns $ parsedHrds++[(k,v)]
     where k = takeWhile (/=':') l
           v = tail $ dropWhile (/=' ') l
 
+proccessRequest :: Request -> IO (String, ResponseBody)
+proccessRequest (Request rqst hdr bdy) =
+    case rMethod rqst of
+        GET -> proccessGET $ tail $ rURI rqst
+        POST -> proccessPOST (tail $ rURI(rqst)) hdr bdy
+        PUT -> undefined 
+        DELETE -> undefined
+        UNDEFINED -> return ("405",reasonPhrase "405")
+
+-- proccessGET :: URI -> ( Status Code , ResponseBody )
+proccessGET :: String -> IO (String, ResponseBody)
+proccessGET resource = do
+    let filePath = "./resources/" ++ resource
+    let rFilePath = B.pack filePath
+    resourceExist <- fileExist rFilePath
+    if resourceExist
+        then do
+            fileStatus <- getFileStatus rFilePath
+            if isDirectory fileStatus
+                then do
+                    dirContents <- getDirectoryContents filePath
+                    if (resource == "")
+                        then do
+                            let path = resource
+                            let resources = map (path ++) $ drop 2 dirContents
+                            rTyped <- resourcesTyped resources []
+                            return ("200", resourcesToJSON rTyped)
+                        else do
+                            let path = resource ++ "/"
+                            let resources = map (path ++) $ drop 2 dirContents
+                            rTyped <- resourcesTyped resources []
+                            return ("200", resourcesToJSON rTyped)
+                else do
+                    content <- readFile filePath
+                    return ("200", content)
+        else return ("404", reasonPhrase "404")
+
+resourcesTyped :: [String] -> [Resource] -> IO [Resource]
+resourcesTyped [] accum = return accum
+resourcesTyped (r:rs) accum = do
+    let filePath = "./resources/" ++ r
+    let rFilePath = B.pack filePath
+    fileStatus <- getFileStatus rFilePath
+    if isDirectory fileStatus
+        then do
+            resourcesTyped rs ((Set { sName = T.pack r }):accum)
+        else do 
+            resourcesTyped rs ((Event { eName = T.pack r }):accum)
+
+resourcesToJSON :: [Resource] -> String
+resourcesToJSON r = BL.unpack $ encode r
+
+proccessPOST :: String -> RequestHeaders -> RequestBody -> IO (String, ResponseBody)
+proccessPOST resource hdrs bdy = do
+    writeFile ("./resources/" ++ resource) ((rqstHdrsToString hdrs []) ++ bdy)
+    return ("201",reasonPhrase "201")
+
 -- Generate response
-buildResponse :: Request -> Response
-buildResponse request@(Request rqst hdr bdy) = 
+buildResponse :: (String, ResponseBody) -> Response
+buildResponse (code,bdy) = 
     let contype = "application/json"
         getDate = unsafePerformIO $ fmap show getCurrentTime
-    in case (rMethod(rqst)) of 
-        GET -> Response
-               ResponseHeaders { statusCode = "200", resDate = getDate, resConType = contype, resServer = version}
-               ( "{ request }\n" ++ show request )
-        POST -> Response
-               ResponseHeaders { statusCode = "201", resDate = getDate, resConType = contype, resServer = version}
-               ( "{ request }\n" ++ show request )
-        PUT -> Response
-               ResponseHeaders { statusCode = "200", resDate = getDate, resConType = contype, resServer = version}
-               ( "{ request }\n" ++ show request )
-        DELETE -> Response
-               ResponseHeaders { statusCode = "200", resDate = getDate, resConType = contype, resServer = version}
-               ( "{ request }\n" ++ show request )
-        UNDEFINED -> Response
-               ResponseHeaders { statusCode = "405", resDate = getDate, resConType = contype, resServer = version}
-               ( "{ request }\n" ++ show request )
+    in (Response
+        ResponseHeaders { statusCode = code, resDate = getDate, resConType = contype, resServer = version}
+        bdy)
 
 -- Send Response back to client
 reply :: Handle -> Response -> IO ()
 reply handle rsp@(Response hdr bdy) = do
     hPutStr handle $ show rsp 
-    hPutStr handle $ "\n{ response }\n" ++ show hdr
-    return ()
+    -- (debug) hPutStr handle $ "\n{ response }\n" ++ show hdr
